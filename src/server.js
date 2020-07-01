a9a2b3a15c6d3a738dd04745741fe0d8051f9ae2
tpember <tpember@tapqa.com>
2020-07-01 14:27:07 -0500

    reinstalled everything

--- src/server.js
+++ src/server.js
@@ -1,11 +1,42 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var clientAllocations = __importStar(require("./json/clientAllocations.json"));
+var clients = __importStar(require("./json/clients.json"));
+var consultants = __importStar(require("./json/consultants.json"));
+var cors_1 = __importDefault(require("cors"));
 var app = express_1.default();
-app.get('/', function (req, res, next) {
-    res.send('Hello');
+app.use(cors_1.default());
+app.get('/api/allocations', function (req, res, next) {
+    res.send(clientAllocations);
 });
-app.listen(5000, function () { return console.log('Server running'); });
+app.get('/api/clients', function (req, res, next) {
+    res.send(clients);
+});
+app.get('/api/consultants', function (req, res, next) {
+    res.send(consultants);
+});
+var port = 5000;
+app.listen(port, function () { return console.log("Server running on port " + port); });
